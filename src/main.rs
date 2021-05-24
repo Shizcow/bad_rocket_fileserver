e32f81081c42171d3eccf05ded72ceefa7772087
Devin Pohl <pohl.devin@gmail.com>
2021-05-24 17:03:39 -0600

    Repo init, proof of concept for public dir

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+/*
+ * TODO:
+ * public - serves files, shows directories, totally open
+ * private - dirname + password -> public-like interface
+ * secret - prompts for a hash of a file. If that has is found, return the file.
+ * upload - prompts for a unique upload key that determines:
+ *          - allowed number of uploads
+ *          - time window
+ */
+use rocket::{get, response::NamedFile, routes};
+use rocket_contrib::templates::Template;
+
+use std::path::PathBuf;
+
+use serde_derive::Serialize;
+
+#[derive(Serialize)]
+struct TemplateContext {
+    dirname: String,
+    items: Vec<[String; 2]>,
+}
+
+#[get("/public")]
+fn get_public_none() -> Result<Option<NamedFile>, Template> {
+    get_public(PathBuf::from(""))
+}
+
+#[get("/public/<filename..>")]
+fn get_public(filename: PathBuf) -> Result<Option<NamedFile>, Template> {
+    let path_name = PathBuf::from("public").join(filename.clone());
+    let smbroot = PathBuf::from("/home/notroot/smbshare");
+    let fdir = smbroot.join(path_name.clone());
+
+    if fdir.is_dir() {
+        let context = TemplateContext {
+            dirname: path_name.to_string_lossy().to_string(),
+            items: fdir
+                .read_dir()
+                .expect("could not read directory")
+                .filter_map(|entry| {
+                    if let Ok(entry) = entry {
+                        Some([
+                            entry.file_name().to_string_lossy().to_string(),
+                            filename
+                                .clone()
+                                .join(entry.file_name())
+                                .to_string_lossy()
+                                .to_string(),
+                        ])
+                    } else {
+                        None
+                    }
+                })
+                .collect(),
+        };
+        Err(Template::render("dir", &context))
+    } else {
+        // respond with the file and die
+        Ok(NamedFile::open(fdir).ok())
+    } // 404 otherwise
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![get_public_none, get_public])
+        .attach(Template::fairing())
+        .launch();
+}
